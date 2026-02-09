035dd98255b9c11928f4553abd709ecd1378bb9d
Rinel Benjamim <benjamimrinel192@gmail.com>
2026-02-09 15:45:14 +0100

    re-write code

---
+++ 04 - use strict/app.js
@@ -0,0 +1,17 @@
+"use strict"
+
+/*
+ it means that javascript will just use the features that are
+in the latest version of javascript, and it will not use any
+features that are in the older versions of javascript. 
+This is a good practice to use because it will help you to avoid any bugs that
+may occur when you are using older versions of javascript. 
+It will also help you to write cleaner and more efficient code.
+*/
+
+console.clear()
+
+console.log("========================================")
+let nome = "Rinel"
+console.log(nome)
+console.log("========================================")
