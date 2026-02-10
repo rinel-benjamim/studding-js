97d31a1279b44953c98f5b4902e7e2e202d0b882
Rinel Benjamim <benjamimrinel192@gmail.com>
2026-02-10 14:36:57 +0100

    learn about constants

---
+++ 07 - constants/app.js
@@ -0,0 +1,9 @@
+// CONSTANTES
+// vALORES DEFINIDOS COMO CONSTANTES NÃO PODEM TER SEUS VALORES ALTERADOS
+
+const NAME = "Rinel"
+
+// name = "Benjamim" // TypeError: Assignment to constant variable.
+// É uma boa prática definir constantes com letras maiúsculas, para que seja fácil de identificar que são constantes
+console.clear()
+console.log(NAME)
