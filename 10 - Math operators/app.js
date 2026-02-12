a4013308346885a4e23a67d7de848c8ee3704b33
Rinel Benjamim <benjamimrinel192@gmail.com>
2026-02-12 16:36:05 +0100

    math operators

---
+++ 10 - Math operators/app.js
@@ -0,0 +1,16 @@
+// Operadores matemáticos
+let a = 10;
+let b = 5;
+console.log(a + b); // Soma
+console.log(a - b); // Subtração
+
+console.log(a * b); // Multiplicação
+console.log(a / b); // Divisão
+console.log(a % b); // Resto da divisão
+console.log(a ** b); // Exponenciação
+console.log(a++);// Incremento (aumenta o valor de a em 1)
+console.log(a--);// Decremento (diminui o valor de a em 1)
+console.log(++a);// Incremento pré-fixado (aumenta o valor de a em 1 antes de usá-lo)
+console.log(--a);// Decremento pré-fixado (diminui o valor de a em 1 antes de usá-lo)
+console.log(-a);// Negação (inverte o sinal de a)
+console.log(+a);// Operador de identidade (converte a para um número, se possível)
