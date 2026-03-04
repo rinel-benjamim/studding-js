6268ddde28be48fcc61463a24b17f3cb4e2f1b39
Rinel Benjamim <benjamimrinel192@gmail.com>
2026-03-04 14:05:40 +0100

    cicle while and do while

---
+++ 13 - Ciclos while e do while/app.js
@@ -0,0 +1,23 @@
+console.clear()
+
+
+
+let i = 0;
+console.log("Ciclo while");
+
+// Ciclo while
+
+while (i <= 5){
+    console.log(i);
+    i++;
+}
+console.log("");
+
+let j = 0;
+console.log("Ciclo do while");
+
+// Ciclo do while
+do {
+    console.log(j);
+    j++;
+} while (j <= 5)
