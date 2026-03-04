72ec66d8c449aa7a47b306d56391610f55312187
Rinel Benjamim <benjamimrinel192@gmail.com>
2026-03-04 13:45:11 +0100

    conditionals

---
+++ 12 - Instruções Condicionais/app.js
@@ -0,0 +1,31 @@
+// Instruções condicionais
+
+/**
+ * if (condição) {
+ *  // código a ser executado se a condição for verdadeira
+ * }
+*/
+
+console.clear();
+
+let valor_a = 20;
+let valor_b = 10;
+
+/*
+
+if (valor_a < valor_b) {
+    console.log("Valor A é menor que Valor B");
+}else if(valor_a > valor_b){
+    console.log("Valor A é maior que B")
+}else{
+    console.log("Valor A é igual à B")
+}
+**/
+
+// Conditional Operator (Ternário)
+
+let nome = "Rinel"
+
+let verificacao = nome === "Rinel" ? "Sim" : "Não";
+
+console.log(" O utilizador está atorizado? " + verificacao);
